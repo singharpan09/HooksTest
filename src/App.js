41843187737c10f8f946bcd40654ecf81ccc7e47
singharpan09 <arpan@excellencetechnologies.info>
2020-05-20 15:13:25 +0530

    few things added

--- src/App.js
+++ src/App.js
@@ -3,20 +3,25 @@ import "./App.css";
 
 function App() {
   const [joke, setjoke] = useState("loading");
-  //initlially joke will show as loading ...then will set it
-  useEffect(() => {
-    fetch("http://api.icndb.com/jokes/random?firstName=John&amp;lastName=Doe")
+  const newJoke = () => {
+    fetch("http://api.icndb.com/jokes/random?firstName=John&lastName=Doe")
       .then((res) => res.json())
       .then((res2) => {
         console.log(res2);
         setjoke(res2.value.joke);
       });
+  };
+  useEffect(() => {
+    newJoke();
   }, []);
-  //setjoke method will be called in useEffect..when data get fetched
+  //initllly newJoke will be called when Componentmount
+  //then will fetch new joke onCLick event
+  //now we will fetch joke with our given name
   return (
     <React.Fragment>
       <div className="App">
         <h1>This is Test App</h1>
+        <button onClick={() => newJoke()}>Get New Joke</button>
         <h4>{joke}</h4>
       </div>
     </React.Fragment>
