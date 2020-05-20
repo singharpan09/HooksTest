c8009082569edf9b208d617b1f3b6fc29a5596ff
singharpan09 <arpan@excellencetechnologies.info>
2020-05-20 11:58:33 +0530

    multiple states initlized

--- src/App.js
+++ src/App.js
@@ -3,26 +3,22 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [data, setdata] = useState({
-    name: "Mukesh",
-    age: 21,
-  });
+  const [name, setName] = useState("Mukesh");
+  const [age, setAge] = useState(21);
+
   return (
     <div className="App">
       <h2>This is React Hooks</h2>
-      <p>{data.name}</p>
-      <p>{data.age}</p>
-      <button onClick={() => setdata({ ...data, name: "Rahul" })}>
+      <p>{name}</p>
+      <p>{age}</p>
+      {/* <button onClick={() => setdata({ ...data, name: "Rahul" })}>
         change Name
-      </button>
+      </button> */}
     </div>
   );
 }
 
 export default App;
-//this is like setState to change the state
-//but here is a problem...while changing the name here...age data will be lost
-//this problem can be resolved by first spreading the state,then overriding the name property
-//but this is not a good approach
-//That's why here comes the approch of "Multiple states" in hooks
-//we can make multiple states in hooks
+//now in hooks state do not necessarly be javascript object
+//there can be multiple state
+//state can be number,string,boolean,arrya..etc
