eff6c67fadd39a6b2f52d91ec6b591a3b49380b0
singharpan09 <arpan@excellencetechnologies.info>
2020-05-20 11:54:14 +0530

    change name & need of multiple state

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ function App() {
       <h2>This is React Hooks</h2>
       <p>{data.name}</p>
       <p>{data.age}</p>
-      <button onClick={() => setdata({ name: "Rahul" })}>change Name</button>
+      <button onClick={() => setdata({ ...data, name: "Rahul" })}>
+        change Name
+      </button>
     </div>
   );
 }
@@ -20,3 +22,7 @@ function App() {
 export default App;
 //this is like setState to change the state
 //but here is a problem...while changing the name here...age data will be lost
+//this problem can be resolved by first spreading the state,then overriding the name property
+//but this is not a good approach
+//That's why here comes the approch of "Multiple states" in hooks
+//we can make multiple states in hooks
