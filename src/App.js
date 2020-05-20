c2dfd2040cba47913d5dbe12c7e867e1f305280b
singharpan09 <arpan@excellencetechnologies.info>
2020-05-20 12:18:25 +0530

    use of previous state

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import "./App.css";
 
 function App() {
   const [name, setName] = useState("Mukesh");
-  const [age, setAge] = useState(21);
-
+  const [no, setNo] = useState(21);
+  //Clicking on increment button should increase number by "3"
+  //but this is not happing...setNo() is not always getting the previous updated state
   const Increment = () => {
-    setAge(age + 1);
+    setNo(no + 1);
+    setNo(no + 1);
+    setNo(no + 1);
   };
   return (
     <div className="App">
       <h2>This is React Hooks</h2>
       <p>{name}</p>
-      <p>{age}</p>
+      <p>{no}</p>
       <button onClick={() => setName("Rahul")}>change Name</button>
       <button onClick={() => Increment()}>Increase age</button>
     </div>
@@ -21,6 +24,7 @@ function App() {
 }
 
 export default App;
+//functional component don't use "this" keyword
 //now in hooks state do not necessarly be javascript object
 //there can be multiple state
 //state can be number,string,boolean,arrya..etc
