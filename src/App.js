a0888f9e4b85900ca86bce4654b49a895caca770
singharpan09 <arpan@excellencetechnologies.info>
2020-05-20 12:44:47 +0530

    few useEffect comment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 //useEffect is "Mixer" of "ComponentDidMount" and "ComponentDidUpdate"
 //this method involves itself like ComponentDidMount and called Everytime whenever any state update
+//useEffect is a callback function
+//But some time this behiour of useEffect can create problem..like when we are fetching some data..and then fetching another data from another API
+//First API will be called again beacuse of its featuer like "ComponetDdiUpdate"
+//so we need to change it's behavoiur as per our need
 import "./App.css";
 
 function App() {
