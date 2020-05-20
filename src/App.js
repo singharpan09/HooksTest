7b1e7a20bdc7483e3300064ecb8009c8c19c4af6
singharpan09 <arpan@excellencetechnologies.info>
2020-05-20 14:32:26 +0530

    useeffect as componentWillUnmount

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import "./App.css";
 
 const Home = () => {
   useEffect(() => {
-    console.log("useEffect called");
+    return () => {
+      //code cleanup code is written here
+      console.log("ComponentWillUnmount");
+    };
   }, []);
   //we are calling useEffect in homw component
   //this is now equivalent to ComponentDidMount
+  //ab hum function return karwayege "useEffect" se
+  //now this is equivalent to "ComponentWillUnmount"(when useEffect returns a function)
+  //componenetWillUnmount is called when we want to clean memory
+  //and component is not of use..when componenet is to be destroy...the last cycle of component
+  //this is the last method
+  //when we will go from Home component to About component ..this will be called...b'coz Home get destroy
+  //this will be last method that run before destroying the component
   return (
     <React.Fragment>
       <h3>This is Home Component</h3>
