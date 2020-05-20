130c2c7be3be865faf47c2a11bed292967dfa116
singharpan09 <arpan@excellencetechnologies.info>
2020-05-20 15:24:18 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -23,13 +23,23 @@ function App() {
     <React.Fragment>
       <div className="App">
         <h1>This is Test App</h1>
+        <input
+          type="text"
+          value={fname}
+          onChange={(e) => setfname(e.target.value)}
+        />
+        <input
+          type="text"
+          value={lname}
+          onChange={(e) => setlname(e.target.value)}
+        />
         <button onClick={() => newJoke(fname, lname)}>Get New Joke</button>
         <h4>{joke}</h4>
       </div>
     </React.Fragment>
   );
 }
-
+//name passed from input field to joke API
 export default App;
 
 //http://www.icndb.com/api/
