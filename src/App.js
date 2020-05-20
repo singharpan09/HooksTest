ba6feef7dc3baa3ecb383f4ba2704aecc2e9baee
singharpan09 <arpan@excellencetechnologies.info>
2020-05-20 11:10:18 +0530

    readme changed

--- src/App.js
+++ src/App.js
@@ -11,3 +11,8 @@ function App() {
 }
 
 export default App;
+//Hooks is a new feature that allow to use State without using Class based Component
+//till now we were thinking State as an javaScript object
+//React Hooks changed the Concept of state as javacript object
+//Now,in React Hooks...state can be anything...Array,Object,string,number,boolean....
+//We can have Multiples state in React Hooks
