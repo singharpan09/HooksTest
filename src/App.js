68d3d0014a949ed53eeb6f46335d72d1b73aa88a
singharpan09 <arpan@excellencetechnologies.info>
2020-05-20 15:02:31 +0530

    data fethed using useeffect and set using useState

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ function App() {
   useEffect(() => {
     fetch("http://api.icndb.com/jokes/random?firstName=John&amp;lastName=Doe")
       .then((res) => res.json())
-      .then((res2) => console.log(res2));
+      .then((res2) => {
+        console.log(res2);
+        setjoke(res2.value.joke);
+      });
   }, []);
-  //fetching of random joke using "useEffect"
-  //this will be called infinite times
-  //so we will make it behave like componentDidMount
+  //setjoke method will be called in useEffect..when data get fetched
   return (
     <React.Fragment>
       <div className="App">
