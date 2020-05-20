f7f4098b95fb3a3cfa17bc5f76c25e438d061cbf
singharpan09 <arpan@excellencetechnologies.info>
2020-05-20 14:59:02 +0530

    useState initilized

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [joke, setjoke] = useState("loading");
+  //initlially joke will show as loading ...then will set it
   useEffect(() => {
     fetch("http://api.icndb.com/jokes/random?firstName=John&amp;lastName=Doe")
       .then((res) => res.json())
@@ -12,7 +14,10 @@ function App() {
   //so we will make it behave like componentDidMount
   return (
     <React.Fragment>
-      <h1>This is Test App</h1>
+      <div className="App">
+        <h1>This is Test App</h1>
+        <h4>{joke}</h4>
+      </div>
     </React.Fragment>
   );
 }
