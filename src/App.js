740d1d7d3ca92e663f745e950e06ac9ad3910358
singharpan09 <arpan@excellencetechnologies.info>
2020-05-20 14:55:40 +0530

    few useEffect comments

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ function App() {
     fetch("http://api.icndb.com/jokes/random?firstName=John&amp;lastName=Doe")
       .then((res) => res.json())
       .then((res2) => console.log(res2));
-  });
+  }, []);
   //fetching of random joke using "useEffect"
+  //this will be called infinite times
+  //so we will make it behave like componentDidMount
   return (
     <React.Fragment>
       <h1>This is Test App</h1>
