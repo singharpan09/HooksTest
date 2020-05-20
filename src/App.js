7a6c958c9588dacfb2f55839bafbdf5ebabe01bb
singharpan09 <arpan@excellencetechnologies.info>
2020-05-20 14:54:06 +0530

    use of useEffect when component mounts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    fetch("http://api.icndb.com/jokes/random?firstName=John&amp;lastName=Doe")
+      .then((res) => res.json())
+      .then((res2) => console.log(res2));
+  });
+  //fetching of random joke using "useEffect"
   return (
     <React.Fragment>
       <h1>This is Test App</h1>
