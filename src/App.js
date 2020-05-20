496a100d4c35a876e3d231f8d54e23d01b717e96
singharpan09 <arpan@excellencetechnologies.info>
2020-05-20 11:30:44 +0530

    data destructuring for pulling out the data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setdata] = useState({
+    name: "Mukesh",
+    age: 21,
+  });
   return (
     <div className="App">
       <h2>This is React Hooks</h2>
@@ -11,4 +15,7 @@ function App() {
 }
 
 export default App;
-//import {useState} from "react" for using state in functional component
+
+//This is basically destructuring for pulling the data (name can be any name)
+//"data" will be used to show the state data
+//"setdata" will be used to change the data (like setState in class component)
