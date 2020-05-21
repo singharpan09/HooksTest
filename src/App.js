2abfc5ca5cdac195cbbb01bf6d58ed8892422b02
singharpan09 <arpan@excellencetechnologies.info>
2020-05-21 11:52:01 +0530

    React Router Hooks initlilzed with materilize css

--- src/App.js
+++ src/App.js
@@ -1,44 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import Ref from "./useRef";
 
 function App() {
-  const [joke, setjoke] = useState("loading");
-  const [fname, setfname] = useState("john");
-  const [lname, setlname] = useState("doe");
-  const newJoke = (firstname, lastname) => {
-    fetch(
-      `http://api.icndb.com/jokes/random?firstName=${firstname}&lastName=${lastname}`
-    )
-      .then((res) => res.json())
-      .then((res2) => {
-        console.log(res2);
-        setjoke(res2.value.joke);
-      });
-  };
-  useEffect(() => {
-    newJoke(fname, lname);
-  }, [fname, lname]);
-
-  //now useEffect will call the API whenever any change in "fname" or "lname" happens
-  //we can use more than one useEffect according to our needs
   return (
     <React.Fragment>
       <div className="App">
-        <Ref />
         <h1>This is Test App</h1>
-        <input
-          type="text"
-          value={fname}
-          onChange={(e) => setfname(e.target.value)}
-        />
-        <input
-          type="text"
-          value={lname}
-          onChange={(e) => setlname(e.target.value)}
-        />
-
-        <h4>{joke}</h4>
       </div>
     </React.Fragment>
   );
