a3713be137380fd4efa87f24afed121d2d399061
singharpan09 <arpan@excellencetechnologies.info>
2020-05-21 11:55:57 +0530

    navbar component inilized

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from "react";
 import "./App.css";
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <React.Fragment>
       <div className="App">
+        <NavBar />
         <h1>This is Test App</h1>
       </div>
     </React.Fragment>
   );
 }
-//now will fetch the new joke without click event
-//name passed from input field to joke API
-export default App;
 
-//http://www.icndb.com/api/
-//API
-//http://api.icndb.com/jokes/random?firstName=John&amp;lastName=Doe
+export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <React.Fragment>
+      <h2>This is Navbar</h2>
+    </React.Fragment>
+  );
+};
+
+export default NavBar;
