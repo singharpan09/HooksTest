1a6e4e0babde5784d9805f8e51afe21aae9ed79b
singharpan09 <arpan@excellencetechnologies.info>
2020-05-21 11:59:25 +0530

    nav bar links used

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,9 +3,26 @@ import React from "react";
 const NavBar = () => {
   return (
     <React.Fragment>
-      <h2>This is Navbar</h2>
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="brand-logo">
+            Logo
+          </a>
+          <ul id="nav-mobile" className="right">
+            <li>
+              <a href="sass.html">Sass</a>
+            </li>
+            <li>
+              <a href="badges.html">Components</a>
+            </li>
+            <li>
+              <a href="collapsible.html">JavaScript</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </React.Fragment>
   );
 };
-
+//https://materializecss.com/navbar.html
 export default NavBar;
