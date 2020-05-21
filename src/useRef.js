353fbff6d879dcf955e931796fb6f08ed09fd09d
singharpan09 <arpan@excellencetechnologies.info>
2020-05-21 11:25:09 +0530

    ref inilized

--- src/useRef.js
+++ src/useRef.js
@@ -1,13 +1,17 @@
 //here will use "useref" hook in functional component
 
 import React, { useref } from "react";
-
+//initial value of ref is assignned as null
 const Ref = () => {
+  const myheading = useref(null);
+  const input = useref(null);
   return (
     <React.Fragment>
-      <h3>this is ref Component</h3>
+      <h3 ref={myheading}>This is ref Title</h3>
+      <input type="text" ref={input} />
     </React.Fragment>
   );
 };
 
+//will try to get heading and input through "useref" in functional component
 export default Ref;
