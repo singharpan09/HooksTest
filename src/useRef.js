13963043e1eece9193c446b285024954598aee67
singharpan09 <arpan@excellencetechnologies.info>
2020-05-21 11:12:24 +0530

    useRef hook intilized

---
+++ src/useRef.js
@@ -0,0 +1,13 @@
+//here will use "useref" hook in functional component
+
+import React, { useref } from "react";
+
+const Ref = () => {
+  return (
+    <React.Fragment>
+      <h3>this is ref Component</h3>
+    </React.Fragment>
+  );
+};
+
+export default Ref;
